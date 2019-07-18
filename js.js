7a695522f8e2d58dd1a6dbed5eb969345ad6a99c
amandinek <karambiziga@gmail.com>
2019-07-18 08:43:37 +0200

    change the male if statement

--- js.js
+++ js.js
@@ -38,19 +38,19 @@ var day = ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7;
     }
 
     if(day==1 && gender=='male'){
-      alert(names[1]);
+      alert(maleNames[1]);
     } else if(day==2 && gender=='male'){
-      alert(names[2]);
+      alert(maleNames[2]);
     } else if(day==3 && gender=='male'){
-      alert(names[3]);
+      alert(maleNames[3]);
     } else if(day==4 && gender=='male'){
-      alert(names[4]);
+      alert(maleNames[4]);
     } else if(day==5 && gender=='male'){
-      alert(names[5]);
+      alert(maleNames[5]);
     } else if(day==6 && gender=='male'){
-      alert(names[6]);
+      alert(maleNames[6]);
     }else{
-      alert(names[0]);
+      alert(maleNames[0]);
     }
 
    
